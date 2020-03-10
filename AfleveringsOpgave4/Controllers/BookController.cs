d67312480b4d99993d01b40c46dcd5556616901e
Osman Bajric <osman_bih@hotmail.com>
2020-03-10 10:30:17 +0100

    Add project files.

---
+++ AfleveringsOpgave4/Controllers/BookController.cs
@@ -0,0 +1,79 @@
+﻿using AfleveringsOpgave4.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AfleveringsOpgave4;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AfleveringsOpgave4.Controllers
+{
+    [Route("api/Book")]
+    [ApiController]
+    public class BookController
+    {
+        //[Route("api/[controller]")]
+       
+       
+            public static readonly List<Book> books = new List<Book>()
+        {
+            new Book("Osman", 500, "1234567891012", "NyBog"),
+            new Book("Franco", 300,"1234567878907","Bog"),
+        };
+
+            // GET: api/Item
+            [HttpGet]
+
+            public IEnumerable<Book> Get()
+            {
+                return books;
+            }
+
+            // GET: api/Item/5
+            //[HttpGet("{id}", Name = "Get")]
+            [HttpGet]
+            [Route("{isbn13}")]
+
+            public Book Get(string isbn13)
+            {
+                return books.Find(i => i.Isbn13 == isbn13);
+            }
+
+            // POST: api/Item
+            [HttpPost]
+            public void Post([FromBody] Book value)
+            {
+                books.Add(value);
+            }
+
+            // PUT: api/Item/5
+            //[HttpPut("{id}")]
+            [HttpPut]
+            [Route("{isbn13}")]
+            public void Put(string isbn13, [FromBody] Book value)
+            {
+                Book book = Get(isbn13);
+                if (book != null)
+                {
+                    book.Forfatter = value.Forfatter;
+                    book.Sidetal = value.Sidetal;
+                    book.Isbn13 = value.Isbn13;
+                    book.Titel = value.Titel;
+                }
+            }
+
+            // DELETE: api/ApiWithActions/5
+            //[HttpDelete("{id}")]
+            [HttpDelete]
+            [Route("{isbn13}")]
+            public void Delete(string isbn13)
+            {
+                Book book = Get(isbn13);
+                books.Remove(book);
+            }
+            
+        }
+}
+
+
---
+++ AfleveringsOpgave4/Model/Book.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace AfleveringsOpgave4.Model
+{
+    public class Book
+    {
+        private string _forfatter;
+        private int _sidetal;
+        private string _isbn13;
+        public string Titel { get; set; }
+
+        public Book()
+        {
+            
+        }
+        public Book(string forfatter, int sidetal, string isbn13, string titel)
+        {
+            forfatter = forfatter;
+            sidetal = sidetal;
+            isbn13 = isbn13;
+            titel = titel;
+        }
+        public string Isbn13
+        {
+            get => _isbn13;
+            set
+            {
+                if (value.Length == 13) throw new ArgumentOutOfRangeException();
+                {
+                    _isbn13 = value;
+                }
+
+            }
+        }
+
+        public string Forfatter
+        {
+            get => _forfatter;
+            set
+            {
+                if (value.Length < 2) throw new ArgumentOutOfRangeException();
+                {
+                    _forfatter = value;
+                }
+
+            }
+        }
+
+        public int Sidetal
+        {
+            get => _sidetal;
+            set
+            {
+                if (value <= 4 || value >= 1000) throw new ArgumentOutOfRangeException();
+                {
+                    _sidetal = value;
+                }
+
+            }
+        }
+    }
+}
